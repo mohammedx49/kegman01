6e8b4b1afb7b7d75b0acabc6297ddc7112488092
darknight111 <75706333+darknight111@users.noreply.github.com>
2021-01-03 06:30:37 -0800

    Update driver_monitor.py

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -17,13 +17,13 @@ EventName = car.CarEvent.EventName
 #  We recommend that you do not change these numbers from the defaults.
 # ******************************************************************************************
 
-_AWARENESS_TIME = 35.  # passive wheel touch total timeout
-_AWARENESS_TIME = min(int(kegman.conf['wheelTouchSeconds']), 99999)    # x minutes limit without user touching steering wheels make the car enter a terminal status
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 12.
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-_DISTRACTED_TIME = 11.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+_AWARENESS_TIME = 99999.  # passive wheel touch total timeout
+_AWARENESS_TIME = min(int(kegman.conf['wheelTouchSeconds']), 999999)    # x minutes limit without user touching steering wheels make the car enter a terminal status
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 99999.
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 99999.
+_DISTRACTED_TIME = 99999.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 99999.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 99999.
 
 _FACE_THRESHOLD = 0.6
 _EYE_THRESHOLD = 0.6
@@ -51,7 +51,7 @@ _POSE_OFFSET_MAX_COUNT = 3600  # stop deweighting new data after 6 min, aka "sho
 _RECOVERY_FACTOR_MAX = 5.  # relative to minus step change
 _RECOVERY_FACTOR_MIN = 1.25  # relative to minus step change
 
-MAX_TERMINAL_ALERTS = 3  # not allowed to engage after 3 terminal alerts
+MAX_TERMINAL_ALERTS = 999  # not allowed to engage after 3 terminal alerts
 MAX_TERMINAL_DURATION = 300  # 30s
 
 # model output refers to center of cropped image, so need to apply the x displacement offset
