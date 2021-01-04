420089ed0e5a3da470329be797e26706617f9c2c
darknight111 <75706333+darknight111@users.noreply.github.com>
2021-01-03 18:12:12 -0800

    Update driver_monitor.py

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -17,13 +17,13 @@ EventName = car.CarEvent.EventName
 #  We recommend that you do not change these numbers from the defaults.
 # ******************************************************************************************
 
-_AWARENESS_TIME = 99999.  # passive wheel touch total timeout
+_AWARENESS_TIME = 35.  # passive wheel touch total timeout
 _AWARENESS_TIME = min(int(kegman.conf['wheelTouchSeconds']), 999999)    # x minutes limit without user touching steering wheels make the car enter a terminal status
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 99999.
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 99999.
-_DISTRACTED_TIME = 99999.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 99999.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 99999.
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 12.
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
+_DISTRACTED_TIME = 11.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
 
 _FACE_THRESHOLD = 0.6
 _EYE_THRESHOLD = 0.6
